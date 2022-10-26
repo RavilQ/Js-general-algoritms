6d71bda119955263b905702d38793b7070edfe20
Ravil <ravil.quliyev.98@bk.ru>
2022-10-26 10:26:43 +0400

    Tapsiriq

---
+++ New folder2/assets/main.js
@@ -0,0 +1,77 @@
+
+////////////////////// 1-ci tapsirigin helli ///////////////////////////////////////////////
+
+// let getarr = [1,8,2,3,3,4,5,4,6,7,5,8,9,10];
+// let myarr=[];
+
+// for (const items of getarr) {
+//     if (!myarr.includes(items)) {
+//         myarr.push(items);
+//     }
+// }
+
+
+// console.log(myarr);
+
+
+
+
+
+     
+
+
+
+
+
+    
+ 
+
+
+/////////////////////  2-ci ev tapsitigi  ////////////////////////////////////////
+
+// let count=0;
+// let arr =[-57,2,3,-4,5,6,7,-8,9];
+// let newarr =[];
+
+
+// arr.reduce((first_item,reduce_array)=>{
+    
+    
+//     if (reduce_array>0) {
+//         count=reduce_array*reduce_array;
+//         newarr.push(count);
+//     }
+    
+    
+// },0)
+// console.log(newarr);
+
+/////////////////////////////////// 3-cu ev tapsiriginin 1-ci helli //////////////////////////////////////////
+
+
+// let test = "Spiderman";
+
+// let answer = test.split("").reverse().join("");
+
+// console.log(answer);
+
+
+/////////////////////////////////// 3-cu ev tapsiriginin 2-ci helli //////////////////////////////////////////
+
+// let word = "Avengers";
+
+// let word_arr = word.split("");
+
+// let newarray =[];
+
+// let i=1;
+
+// word_arr.reduce((acc,items)=>{
+    
+    
+//     newarray.push(word_arr[word_arr.length-i]);
+//     i++;
+
+// },0)
+
+// console.log(newarray.join(""));
